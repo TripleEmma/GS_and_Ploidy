4feb4c8e6b09d1b61d08e82f46a7190b222c727f
TripleEmma <92750444+TripleEmma@users.noreply.github.com>
2024-03-26 14:00:22 +0100

    Update 07_global_community_analysis_simple_lmer.R

--- 07_global_community_analysis_simple_lmer.R
+++ 07_global_community_analysis_simple_lmer.R
@@ -5,7 +5,6 @@
 # (discussion section; region is taken as random intercept)
 
 ## Contacts ----
-# Chongyi Jiang (chongyi.jiang@uni-jena.de)
 ## Create dates: 2024-01-27 ----
 ## Licence: CC BY 4.0 ----
 
